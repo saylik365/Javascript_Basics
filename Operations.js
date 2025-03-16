b66e1408d83c8862eb17d08234a664c413303c05
saylik365 <saylikulkarni0645@gmail.com>
2025-03-16 16:11:23 +0530

    Added new JavaScript file

---
+++ Operations.js
@@ -0,0 +1,39 @@
+//ecmascript documentation
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) // gives 2 raise to power 3 = 8
+console.log(2/2)
+console.log(2%2)
+
+let str1 = 'hello'
+let str2 =" Sayli"
+
+let str3 = str1 + str2
+console.log(str3)
+
+//tricky addition
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+//tricky nigative value
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+console.log(true); //returns only true
+console.log(+true);// tricky boolean conversion
+console.log(+""); // tricky boolean conversion
+
+let gamecounter = 100
+//prefix and postfix :read from mdn web_docs documentation
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter)
+
+
+
