ef3ed477139eae00a422abd2779d40a95cfa3097
saylik365 <saylikulkarni0645@gmail.com>
2025-04-11 23:53:15 +0530

    modified

--- Strings.js
+++ Strings.js
@@ -1,4 +1,17 @@
+//BASIC STRING DECLARATION/ CONCATENATION
+//String Declaration
 const name = "John Doe";
 const age = 30;
 
 console.log(name + age + "Value"); //Basic String Concatenation
+
+//MODERN WAY OF STRING CONCATENATION/ DECLARATION
+// USING BACKTICS, known as string interpolation
+
+
+const name2 = "John Doe";
+const age2 = 30;
+console.log(`My name is ${name2} and I am ${age2} years old.`); //String Interpolation
+//String Interpolation allows us to use variables inside a string without using + operator
+// In short it creates placeholders for the variables inside the string
+// and replaces them with the actual values of the variables.
