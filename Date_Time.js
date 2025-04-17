aff8cfab3e6df63feeae34b6b928b41818d28368
saylik365 <saylikulkarni0645@gmail.com>
2025-04-17 23:18:16 +0530

    commit

---
+++ Date_Time.js
@@ -0,0 +1,5 @@
+//Js has a already defined API for date but there is a new API called Temporal
+// Temporal API is a new API for working with dates and times in JavaScript.
+// It provides a more powerful and flexible way to handle dates and times than the existing Date object.
+// The Temporal API is designed to be more accurate, easier to use, and more consistent across different time zones and locales.
+// The Temporal API is still in development and is not yet fully supported in all browsers.
