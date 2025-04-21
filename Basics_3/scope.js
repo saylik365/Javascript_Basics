cdd7bb83796381fc6b63e5105473b620d710706f
saylik365 <saylikulkarni0645@gmail.com>
2025-04-21 23:52:55 +0530

    commit

---
+++ Basics_3/scope.js
@@ -0,0 +1,30 @@
+// Global scope
+
+let a = 10
+//let a = 10;
+// const b = 20
+// var c = 30 
+
+// var, let, const are all perform same function but they have different scope
+
+//block scope
+if(true){
+    let a = 100
+    console.log("INNER: ",a) // 100
+   
+}
+if (true){
+    const b = 200
+}
+
+// if(true){
+//     var c = 300
+// }
+
+
+// console.log(a)// console.log(b) // ReferenceError: b is not defined
+// console.log(b)// ReferenceError: b is not defined
+// console.log(c) // 300
+console.log(a) // 100
+
+//the scope declared in console is different from the scope declared in the nodejs file
