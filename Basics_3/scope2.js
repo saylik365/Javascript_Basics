d9e111c5b4058d66f9cf087b27045f1643323eb1
saylik365 <saylikulkarni0645@gmail.com>
2025-04-22 23:06:42 +0530

    commit

--- Basics_3/scope2.js
+++ Basics_3/scope2.js
@@ -2,7 +2,10 @@ function one(){
     const username = "Sayli"
     
     function two(){
-        
+        const website = "sayli.com"
+        console.log(username) // Sayli because two() is inside one(), so one() is global for two()
     }
-
+    console.log(website) // Error: website is not defined because website is local to two() and not accessible here
+    two() // Call the inner function to access the outer variable
 }
+one();
