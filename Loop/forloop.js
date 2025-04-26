61728fdf801b03413a340d0b9463787329bf36fe
saylik365 <saylikulkarni0645@gmail.com>
2025-04-26 21:21:22 +0530

    modified

--- Loop/forloop.js
+++ Loop/forloop.js
@@ -3,22 +3,23 @@
 //     console.log(element);
 // }
     
-// for (let i = 0; i <= 10; i++) {
-//     const element = i; // it will print 0 to 4
-//     if(element == 5){
-//         console.log("5 is best number");
-//     }//if block will get execute 
+for (let i = 0; i <= 10; i++) {
+    const element = i; // it will print 0 to 4
+    if(element == 5){
+        console.log("5 is best number");
+    }//if block will get execute 
 
-//     console.log(element); //then it will print 5 to 10
-// }
+    console.log(element); //then it will print 5 to 10
+}
 
-// for (let i = 0; i < 10; i++) {
-//     console.log(`Outer loop value: ${i}`); //it will print outer loop value
-//     for (let j = 0; j < 10; j++) {
-//         console.log(`Inner loop value: ${j} and outer loop value: ${i}`);
-//     }
+/* for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value: ${i}`); //it will print outer loop value
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner loop value: ${j} and outer loop value: ${i}`);
+    }
     
-// }
+}*/
+
 //it will print one outer loop value first then go to inner loop, execute inner loop 10 times, then again go to outer loop
 //print outer loop's 2nd value.....and so on
     
@@ -47,20 +48,20 @@ Inner loop value: 9 and outer loop value: 1*/
 
 
 //multiplication table
-// for (let i = 1; i<=10; i++) {
-//     console.log(`Outer loop value: ${i}`); //it will print outer loop value
-//     for (let j = 1; j <= 10; j++) {
-//         console.log(`${i} * ${j} = ${i * j}`);
-//     }
-// }
+for (let i = 1; i<=10; i++) {
+    console.log(`Outer loop value: ${i}`); //it will print outer loop value
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
 
 //array
-// let myarray= ["flash", "superman","batsman"]
-// console.log(myarray.length); //it will print length of array
-// for (let i = 0; i < myarray.length; i++) {
-//     const element = myarray[i];
-//     console.log(element);
-// }
+let myarray= ["flash", "superman","batsman"]
+console.log(myarray.length); //it will print length of array
+for (let i = 0; i < myarray.length; i++) {
+    const element = myarray[i];
+    console.log(element);
+}
 
 // +++++++++++++++++++ break statement ++++++++++++++++++
 
@@ -77,8 +78,11 @@ for(let i= 1; i<=20; i++){
     }
     console.log(`Value of i is: ${i}`)
 }
+
 console.log("\n")
- console.log("use of continue statement")
+console.log("use of continue statement")
+
+
 //++++++++++++++ continue statement +++++++++++++++++
 
 //The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop. 
